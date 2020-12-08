d6b85d73aabae6b1cb910c28b8450bc9b6367453
Valery Shel <valeryshel@gmail.com>
2020-12-08 12:22:03 +0300

    update gulp

--- gulp-tasks/webp.js
+++ gulp-tasks/webp.js
@@ -5,6 +5,7 @@ import gulp from "gulp";
 import gulpif from "gulp-if";
 import imageminWebp from "imagemin-webp";
 import webp from "gulp-webp";
+import newer from "gulp-newer";
 import debug from "gulp-debug";
 import browsersync from "browser-sync";
 import yargs from "yargs";
@@ -13,13 +14,14 @@ const argv = yargs.argv,
     production = !!argv.production;
 
 gulp.task("webp", () => {
-    return gulp.src(paths.webp.src)
+    return gulp.src(paths.images.src)
+        .pipe(newer(paths.images.dist))
         .pipe(webp(gulpif(production, imageminWebp({
             lossless: true,
             quality: 100,
             alphaQuality: 100
         }))))
-        .pipe(gulp.dest(paths.webp.dist))
+        .pipe(gulp.dest(paths.images.dist))
         .pipe(debug({
             "title": "Images"
         }))
